3069cbfc6c275e1764abba7843ab4494afcf6cde
Kris J <kristoffer.juarez@gmail.com>
2026-04-16 20:47:21 -0400

    1. Analyze the existing translation logic for each sensor to identify common patterns and unique parameters. 2. Create a generalized translation function that takes `txid` and sensor-specific parameters. 3. Define a mapping of sensor choices to their respective parameters. 4. Refactor `txid_translation.py` by replacing the repetitive functions with the generalized one and updating `main`. 5. Verify the changes using `test_conversion.py` (if it exists and is relevant) or by running the script manually. 6. Finalize the code by cleaning up comments and ensuring consistent style.

--- txid_translation.py
+++ txid_translation.py
@@ -1,391 +1,110 @@
-#Translate the TXID for the DW-10 external loop Final TXID for Risco
-def DW10_External( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '20' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '2' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"
-    else:
-        print "Got a false value!"
-
-
-#Translate the TXID for the DW-10 magnet loop for the Final TXID for Risco
-def DW10_Magnet( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '20' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('0160', txid))
-        print "Translated TXID: ",txid, "\n\n"
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '2' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('0160', txid))
-        print "Translated TXID: ",txid, "\n\n"
-    else:
-        print "Got a false value!"    
+# Translate the TXID for various sensors for Risco
+def translate_txid(txid, insert_val, prefix, threshold=5):
+    hex_txid = hex(txid)
+    payload_len = len(hex_txid) - 2
     
-
-#Translate the TXID for the SMKT3 to the Final TXID for Risco
-def SMKT3( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '10' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '1' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"
+    if payload_len < threshold:
+        # For small values, insert value + '0'
+        new_hex = hex_txid[:2] + str(insert_val) + '0' + hex_txid[2:]
     else:
-        print "Got a false value!"
+        # For large values, just insert value
+        new_hex = hex_txid[:2] + str(insert_val) + hex_txid[2:]
     
+    translated = str(int(new_hex, 16))
+    final_txid = prefix + translated
+    print(f"Translated TXID:  {final_txid} \n\n")
 
-#Translate the TXID for the GB1 to the Final TXID for Risco
-def GB1( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '50' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('0160', txid))
-        print "Translated TXID: ",txid, "\n\n"      
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '5' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('0160', txid))
-        print "Translated TXID: ",txid, "\n\n"     
+# Configuration for sensors: (insert_val, prefix, threshold)
+# NOTE: KEY2_PANIC1 and TILT1 have special logic and are handled via dedicated functions.
+
+# KEY2_PANIC1 has a weird branch in original:
+# elif len(txid)-2 >= 6:
+#     txid = "".join(('048', txid)) # notice '048' instead of '0480'
+
+# TILT1 also has a weird branch:
+# elif len(txid)-2 >= 5:
+#     txid = "".join(('112', txid)) # notice '112' instead of '1120'
+
+def DW10_Magnet(txid): translate_txid(txid, 2, '0160')
+def DW10_External(txid): translate_txid(txid, 2, '1120')
+def SMKT3(txid): translate_txid(txid, 1, '1120')
+def SMKT3_Heat(txid): translate_txid(txid, 9, '1120')
+def SMKT3_Freeze(txid): translate_txid(txid, 8, '1120')
+def GB1(txid): translate_txid(txid, 5, '0160')
+def PIR1(txid): translate_txid(txid, 0, '0160')
+def FT1(txid): translate_txid(txid, 4, '1120')
+def FT1_Heat(txid): translate_txid(txid, 9, '1120')
+def FT1_Freeze(txid): translate_txid(txid, 8, '1120')
+def CO3(txid): translate_txid(txid, 7, '1120')
+def HW_SHOCK(txid): translate_txid(txid, 3, '1120')
+
+def KEY2_PANIC1(txid):
+    hex_txid = hex(txid)
+    if len(hex_txid) - 2 < 6:
+        new_hex = hex_txid[:2] + '00' + hex_txid[2:]
+        prefix = '0480'
     else:
-        print("Got a false value!")
-    
+        new_hex = hex_txid[:2] + '0' + hex_txid[2:]
+        prefix = '048'
+    translated = str(int(new_hex, 16))
+    print(f"Translated TXID:  {prefix + translated} \n\n")
 
-
-
-#Translate the TXID for the PIR to the Final TXID for Risco
-def PIR1( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '00' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('0160', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '0' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('0160', txid))
-        print "Translated TXID: ",txid, "\n\n"     
+def TILT1(txid):
+    hex_txid = hex(txid)
+    if len(hex_txid) - 2 < 5:
+        new_hex = hex_txid[:2] + 'C0' + hex_txid[2:]
+        prefix = '1120'
     else:
-        print("Got a false value!")
-    
+        new_hex = hex_txid[:2] + 'C' + hex_txid[2:]
+        prefix = '112'
+    translated = str(int(new_hex, 16))
+    print(f"Translated TXID:  {prefix + translated} \n\n")
 
-
-
-#Translate the TXID for the key2 and panic to the Final TXID for Risco
-def KEY2_PANIC1( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 6:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '00' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('0480', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 6:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '0' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('048', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")    
-    
-
-
-
-#Translate the TXID for the FT1 to the Final TXID for Risco
-def FT1( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '40' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '4' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")    
-
-
-#Translate the TXID for the SMKT3 Heat with heat loop to the Final TXID for Risco
-def SMKT3_Heat( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '90' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '9' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")      
-        
-        
-#Translate the TXID for the FT1 with heat loop to the Final TXID for Risco
-def FT1_Heat( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '90' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '9' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")      
-
-
-
-#Translate the TXID for the FT1 with heat loop to the Final TXID for Risco
-def HW_SHOCK( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '30' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '3' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")    
-
-#Error Handler to handle incorrect input
 def errhandler():
-    print ("Thats incorrect input. Try again: ")
+    print("Thats incorrect input. Try again: ")
 
-#Translate the TXID for any sensor that involves freeze with heat loop to the Final TXID for Risco
-def SMKT3_Freeze( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '80' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '8' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")       
-        
-        
-#Translate the TXID for any sensor that involves freeze with heat loop to the Final TXID for Risco
-def FT1_Freeze( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '80' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '8' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")   
-        
-#Translate the TXID for CO3 to the Final TXID for Risco
-def CO3( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '70' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + '7' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")   
-        
-#Translate the TXID for TILT1 to the Final TXID for Risco
-def TILT1( txid ):
-    txid = hex(txid)
-    if len(txid)-2 < 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + 'C0' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('1120', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    elif len(txid)-2 >= 5:
-        txid = str(txid)
-        index = txid.find('0x')
-        txid = txid[:2] + 'C' + txid[2:]
-        txid = int(txid, 0)
-        txid = str(txid)
-        txid = "".join(('112', txid))
-        print "Translated TXID: ",txid, "\n\n"     
-    else:
-        print("Got a false value!")  
-
-#Get the user input
 def get_input():
-    print("Choose which  sensor you would like to have the TXID translated for:")
-    print("--------------------------------------------------------------------")
-    print("1. DW10 Magnet ")
-    print("2. DW10 External ")
-    print("3. SMKT3 ")
-    print("4. SMKT3 - Heat ")
-    print("5. SMKT3 - Freeze ")
-    print("6. GB1-1 ")
-    print("7. PIR-1 ")
-    print("8. KEY-2, PANIC-1 ")
-    print("9. FT-1 ")
-    print("10. FT-1 - Heat ")
-    print("11. FT-1 - Freeze ")
-    print("12. CO3")
-    print("13. TILT1")
-    print("14. Honeywell Shock Sensors")
-    value=int(raw_input("Enter a choice: "))
+    print("\nChoose which sensor you would like to have the TXID translated for:")
+    print("-" * 68)
+    sensors = [
+        "DW10 Magnet", "DW10 External", "SMKT3", "SMKT3 - Heat", "SMKT3 - Freeze",
+        "GB1-1", "PIR-1", "KEY-2, PANIC-1", "FT-1", "FT-1 - Heat", "FT-1 - Freeze",
+        "CO3", "TILT1", "Honeywell Shock Sensors"
+    ]
+    for i, name in enumerate(sensors, 1):
+        print(f"{i}. {name}")
     
-    if value > 14 or value < 1:
-        print("Exiting... \n")
-        quit()
-    else:
-        return value
+    try:
+        value = int(input("Enter a choice: "))
+        if 1 <= value <= 14:
+            return value
+    except ValueError:
+        pass
+    
+    print("Exiting... \n")
+    quit()
 
-#Main()
 def main():
     value = get_input()
-    txid = raw_input("Enter the TXID value: ")
-    length = len(txid)
-    txid = int(txid)
-    
-    if length == 7:
-        takeaction = {
-            1: DW10_Magnet,
-            2: DW10_External,
-            3: SMKT3,
-            4: SMKT3_Heat,
-            5: SMKT3_Freeze,
-            6: GB1,
-            7: PIR1,
-            8: KEY2_PANIC1,
-            9: FT1,
-            10: FT1_Heat,
-           11: FT1_Freeze,
-           12: CO3,
-           13: TILT1,
-           14: HW_SHOCK,
-           }
-    
-        try:
-            takeaction[value] ( txid )
-        except KeyError:
-            errhandler()
-            main()
-    else:
+    txid_input = input("Enter the TXID value: ")
+    if len(txid_input) != 7:
         print("You gotta enter 7 digits for the TXID. ")
+        return main()
+    
+    try:
+        txid = int(txid_input)
+        actions = {
+            1: DW10_Magnet, 2: DW10_External, 3: SMKT3, 4: SMKT3_Heat, 5: SMKT3_Freeze,
+            6: GB1, 7: PIR1, 8: KEY2_PANIC1, 9: FT1, 10: FT1_Heat, 11: FT1_Freeze,
+            12: CO3, 13: TILT1, 14: HW_SHOCK
+        }
+        actions[value](txid)
+    except ValueError:
+        errhandler()
+    except KeyError:
+        errhandler()
+    
     main()
 
-#Program start
-main()
+if __name__ == "__main__":
+    main()
